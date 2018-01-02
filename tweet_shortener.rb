6bfd18a57faa780410dde7117c45adbe8ed8491b
Christopher Diep <diep.christopher@gmail.com>
2018-01-02 23:43:13 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -40,7 +40,7 @@ def selective_tweet_shortener(tweet)
 end
 
 def shortened_tweet_truncator(tweet)
-  if word_substituter(tweet).length > 140
+  if tweet.length > 140
     word_substituter(tweet)[0..137] + "..."
   else
     tweet
